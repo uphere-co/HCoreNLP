8e64be8a1d1c923961efd487bff49b497ef268e0
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-12 16:05:33 +0000

    @IO -> @_

--- test/callparser.hs
+++ test/callparser.hs
@@ -54,9 +54,9 @@ main = do
       forM_ otxts $ \otxt -> do
         txt <- reflect otxt
         TIO.putStrLn ("Text: " <> otxt)
-        --  @IO and @Int32 are from TypeApplications. [java| ... |] quotation block is polymorphic
+        --  @_ and @Int32 are from TypeApplications. [java| ... |] quotation block is polymorphic
         --  but it cannot be preprocessed if it does not have a concrete type.
-        void @IO @Int32
+        void @_ @Int32
           [java|{
                   String inputFormat = $props.getProperty("inputFormat", "text");
                   String date = $props.getProperty("date");
