36453ed1f459bc2bc28d4131dea8af071bd53cc8
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-17 20:47:55 +0000

    refactor out prepare and annotateTime.

--- src/CoreNLP/SUTime.hs
+++ src/CoreNLP/SUTime.hs
@@ -19,10 +19,8 @@ import           Language.Java.Inline
 import qualified Language.Java                (reflect,reify)
 
 
-sutimeMain :: Text -> IO Text -- Int32
-sutimeMain otxt = do
-  txt <- Language.Java.reflect otxt
-  r <- 
+prepare :: IO (J ('Class "edu.stanford.nlp.pipeline.AnnotationPipeline"))
+prepare = do
     [java|{
             java.util.Properties props = new java.util.Properties();
             edu.stanford.nlp.pipeline.AnnotationPipeline pipeline = new edu.stanford.nlp.pipeline.AnnotationPipeline();
@@ -30,26 +28,29 @@ sutimeMain otxt = do
             pipeline.addAnnotator(new edu.stanford.nlp.pipeline.WordsToSentencesAnnotator(false));
             pipeline.addAnnotator(new edu.stanford.nlp.pipeline.POSTaggerAnnotator(false));
             pipeline.addAnnotator(new edu.stanford.nlp.time.TimeAnnotator("sutime", props));
+            return pipeline;
+          }
+    |]
 
+annotateTime :: J ('Class "edu.stanford.nlp.pipeline.AnnotationPipeline") -> Text -> Text -> IO Text 
+annotateTime pipeline otxt otimetxt = do
+  txt <- Language.Java.reflect otxt
+  timetxt <- Language.Java.reflect otimetxt
+  r <- 
+    [java|{
             String text = $txt;
-
             edu.stanford.nlp.pipeline.Annotation annotation = new edu.stanford.nlp.pipeline.Annotation(text);
-            annotation.set(edu.stanford.nlp.ling.CoreAnnotations.DocDateAnnotation.class, "2017-04-17");
-            pipeline.annotate(annotation);
-            System.out.println("---------------------");
-            System.out.println(annotation.get(edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation.class));
-            System.out.println("---------------------");
+            annotation.set(edu.stanford.nlp.ling.CoreAnnotations.DocDateAnnotation.class, $timetxt);
+            $pipeline.annotate(annotation);
             java.util.List<edu.stanford.nlp.util.CoreMap> timexAnnsAll = annotation.get(edu.stanford.nlp.time.TimeAnnotations.TimexAnnotations.class);
             StringBuilder stringBuilder = new StringBuilder();
 
             for (edu.stanford.nlp.util.CoreMap cm : timexAnnsAll) {
               java.util.List<edu.stanford.nlp.ling.CoreLabel> tokens = cm.get(edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation.class);
-
-              System.out.println(cm + " [from char offset " +
-                tokens.get(0).get(edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetBeginAnnotation.class) +
-                " to " + tokens.get(tokens.size() - 1).get(edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetEndAnnotation.class) + ']' +
-                " --> " + cm.get(edu.stanford.nlp.time.TimeExpression.Annotation.class).getTemporal());
-
+              //System.out.println(cm + " [from char offset " +
+              //  tokens.get(0).get(edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetBeginAnnotation.class) +
+              //  " to " + tokens.get(tokens.size() - 1).get(edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetEndAnnotation.class) + ']' +
+              //  " --> " + cm.get(edu.stanford.nlp.time.TimeExpression.Annotation.class).getTemporal());
               stringBuilder.append(cm);
               stringBuilder.append(",");
               stringBuilder.append(tokens.get(0).get(edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetBeginAnnotation.class));
