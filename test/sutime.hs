b6fe4f5f88278f6c8a6b7efa606a0f70c1df7f93
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-18 01:03:37 +0000

    a little formatting

--- test/sutime.hs
+++ test/sutime.hs
@@ -19,6 +19,7 @@ import           Data.Aeson.Types
 import qualified Data.Attoparsec.Text       as A
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as BL
+import           Data.Monoid
 import           Data.Text                        (Text)
 import qualified Data.Text                  as T
 import qualified Data.Text.IO               as TIO
@@ -26,6 +27,7 @@ import           GHC.Generics
 import           Language.Java         as J
 import           System.Console.Haskeline
 import           System.Environment               (getEnv,getArgs)
+import           Text.Printf 
 --
 import           NLP.SyntaxTree.Parser
 import           NLP.SyntaxTree.Printer
@@ -38,6 +40,9 @@ import           CoreNLP.Type
 
     -- let txt = "Starting next week, Wal-Mart shoppers will receive a discount on 10,000 online-only items if they elect to pick up their orders in-store. Come June, the service will be available on more than 1 million items. The discounts will vary based on the item's size and price."
 
+formatstr n x = T.pack (printf ("%" ++ show n ++ "s") x)
+format x = T.pack (show (tt_coffbeg x)) <> "\t" <> T.pack (show (tt_coffend x)) <> "\t" <> formatstr 20 (tt_txt x) <> "\t" <> tt_timex x
+
 main :: IO ()
 main = do
     args <- getArgs
@@ -48,9 +53,10 @@ main = do
       pp <- prepare
       r <- annotateTime pp txt "2017-04-17"
       TIO.putStrLn r
-      print $ A.parse (many (timetag <* A.skipSpace)) r      
       
       case A.parseOnly (many (timetag <* A.skipSpace)) r of
         Left err -> print err
-        Right xs -> mapM_ print xs
-      
+        Right xs -> do 
+          TIO.putStrLn txt
+          putStrLn "==========================================================="
+          mapM_ (TIO.putStrLn . format) xs
