38e0bb0ab8a12d87786abf3f93f247eb5a4309b1
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-24 05:18:37 +0000

    HCoreNLP is buildable through Nix. Stanford CoreNLP is also packaged as nix package.

---
+++ default.nix
@@ -0,0 +1,24 @@
+{ mkDerivation, aeson, attoparsec, base, bytestring, data-default
+, inline-java, jni, jvm, lens, stdenv, text, transformers
+, jdk
+, corenlp, corenlp_models
+}:
+mkDerivation {
+  pname = "HCoreNLP";
+  version = "0.1.0.0";
+  src = ./.;
+  preBuild = ''
+    export CLASSPATH=${corenlp_models}:${corenlp}/stanford-corenlp-3.7.0.jar:${corenlp}/protobuf.jar:${corenlp}/joda-time.jar:${corenlp}/jollyday.jar
+    echo $CLASSPATH
+  '';
+  libraryHaskellDepends = [
+    aeson attoparsec base bytestring data-default inline-java jni jvm
+    lens text transformers
+  ];
+  buildDepends = [ jdk ];
+  license = "unknown";
+  doHaddock = false;
+  passthru = {
+    CLASSPATH="${corenlp_models}:${corenlp}/stanford-corenlp-3.7.0.jar:${corenlp}/protobuf.jar:${corenlp}/joda-time.jar:${corenlp}/jollyday.jar";
+  };
+}
