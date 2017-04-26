3e1b7fe7a08dc176d34f977ce66fb954348821a3
Ian-Woo Kim <ianwookim@gmail.com>
2017-04-26 21:50:21 +0000

    now understood how to use ProtobufAnnotationSerializer.

--- src/CoreNLP/SUTime.hs
+++ src/CoreNLP/SUTime.hs
@@ -48,16 +48,48 @@ annotateTime pipeline otxt otimetxt = do
             edu.stanford.nlp.pipeline.Annotation annotation = new edu.stanford.nlp.pipeline.Annotation(text);
             annotation.set(edu.stanford.nlp.ling.CoreAnnotations.DocDateAnnotation.class, $timetxt);
             $pipeline.annotate(annotation);
+
+
+            edu.stanford.nlp.pipeline.ProtobufAnnotationSerializer ser = new edu.stanford.nlp.pipeline.ProtobufAnnotationSerializer(false);
+
+            //edu.stanford.nlp.pipeline.CoreNLPProtos.Document doc = ser.toProto(annotation);
+            //System.out.println(doc);
+
+
+
+
             java.util.List<edu.stanford.nlp.util.CoreMap> timexAnnsAll = annotation.get(edu.stanford.nlp.time.TimeAnnotations.TimexAnnotations.class);
             StringBuilder stringBuilder = new StringBuilder();
 
             for (edu.stanford.nlp.util.CoreMap cm : timexAnnsAll) {
+              edu.stanford.nlp.pipeline.CoreNLPProtos.Sentence sent = ser.toProto(cm);
+              System.out.println(sent);
+
+              edu.stanford.nlp.time.Timex timex = cm.get(edu.stanford.nlp.time.TimeAnnotations.TimexAnnotation.class);
+              edu.stanford.nlp.pipeline.CoreNLPProtos.Timex tmx = ser.toProto(timex);
+              System.out.println(tmx);
+              
+
+              // java.util.List<>
+
+              Integer b = cm.get(edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetBeginAnnotation.class);
+              Integer e = cm.get(edu.stanford.nlp.ling.CoreAnnotations.CharacterOffsetEndAnnotation.class);
+              System.out.println(b);
+              System.out.println(e);
+
+
               java.util.List<edu.stanford.nlp.ling.CoreLabel> tokens = cm.get(edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation.class);
+              for(edu.stanford.nlp.ling.CoreLabel token : tokens ) {
+                edu.stanford.nlp.pipeline.CoreNLPProtos.Token tkn = ser.toProto(token);
+                System.out.println(tkn);
+              
+              }
+              
               stringBuilder.append(cm.toShorterString());
               stringBuilder.append("\n");
             }
             String finalString = stringBuilder.toString();
-            return finalString;
+            return finalString; 
           }
     |]
   Language.Java.reify r
