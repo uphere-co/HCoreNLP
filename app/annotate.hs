fe9ce320af4d5d6c775dd3efc3412c683cffd348
Doh Young Mo <modohyoung@gmail.com>
2017-05-13 19:37:02 +0000

    Removed comments.

--- app/annotate.hs
+++ app/annotate.hs
@@ -38,9 +38,6 @@ import qualified CoreNLP.Proto.CoreNLPProtos.Document  as D
 import qualified CoreNLP.Proto.CoreNLPProtos.Sentence  as S
 import qualified CoreNLP.Proto.CoreNLPProtos.Token     as TK
 import qualified CoreNLP.Proto.HCoreNLPProto.ListTimex as T
-
-
-       
     
 {- 
 processTimex ann = do
@@ -83,69 +80,7 @@ processDoc ann = do
           Just newsents = mapM (convertSentence doc) sents
       mapM_ print newsents
       let Just (toklst :: [Token]) = mapM convertToken . concatMap (toListOf (S.token . traverse)) $ sents
-      
-      -- mapM_ print toklst
       return (newsents,toklst)
-{- 
-      print sents
-      putStrLn "show number of tokens per each sentence:"
-      print $ map (lengthOf (S.token . traverse)) sents 
-      putStrLn "show each token"
-      mapM_ print $ zip [1..] $ map (catMaybes . toListOf (S.token . traverse . TK.word)) sents
-      putStrLn "show each timex"
-      mapM_ print $ zip [1..] $ map (catMaybes . toListOf (S.token . traverse . TK.timexValue)) sents
-      putStrLn "show starting point"
-      mapM_ print $ zip [1..] $ map (fromJust . fromJust . firstOf (S.token . traverse . TK.beginChar)) sents
-      putStrLn "show ending point"
-      mapM_ print $ zip [1..] $ map (fromJust . fromJust . lastOf (S.token . traverse . TK.endChar)) sents
-      putStrLn "show the pair of (starting,ending)"
-       -- let  combine :: Lens' s a -> Lens' s b -> Lens' s (a,b)
-       --     combine l1 l2 = lens (\s -> (view l1 s, view l2 s)) (\s (x,y) -> set l1 x (set l2 y s))
-      mapM_ print $ zip [1..] $ flip map sents $ \s -> 
-        let b = fromJust $ fromJust $ firstOf (S.token . traverse . TK.beginChar) s
-            e = fromJust $ fromJust $ lastOf  (S.token . traverse . TK.endChar) s
-        in (b,e)
-      
-      -- print (traverse (D.sentence . S.token) doc)
--}
-
-data TestData = TestData { test :: [TestData2]
-                         } deriving (Show, Generic)
-
-data TestData2 = TestData2 { test2 :: String
-                           } deriving (Show, Generic)
-
-instance FromJSON TestData
-instance FromJSON TestData2
-
-
-
-{-
-data ESOEvent = ESOEvent { NAF :: NafStructure
-                         } deriving (Show, Generic)
-
-data NafStructure = NafStructure { nafHeader :: Header
-                                 , raw :: Text
-                                 , text :: Text
-                                 , terms :: [Term]
-                                 , markables :: [Marks]
-                                 , deps :: [Dep]
-                                 , entities :: [Entity]
-                                 , coreferences :: [Cor]
-                                 , timeExpressions :: [A]
-                                 , srl :: [Srl]
-                                 , constituency :: [Con]
-                                 , temporalRelations :: [Temp]
-                                 , topics :: [Topic]
-                                 , factualities :: [Frac]
-                                 , xml :: Text
-                                 , version :: Text
-                                 } deriving (Show, Generic)
-  
-
-instance FromJSON ESOEvent
--}
-
 
 main :: IO ()
 main = do
@@ -161,9 +96,5 @@ main = do
       (r1, r2) <- processDoc ann 
       let jr1 = encode r1
           jr2 = encode r2
-      print jr1
-      print jr2
-      -- mapM_ print (r1,r2)
-
-    testfile <- decodeFile "test.yml" :: IO (Maybe TestData)
-    print testfile
+      putStrLn (TL.unpack (TLE.decodeUtf8 $ BL.fromStrict jr1))
+      putStrLn (TL.unpack (TLE.decodeUtf8 $ BL.fromStrict jr2))
